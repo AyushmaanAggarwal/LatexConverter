44ebaa80e0aa596dc2d880c596b5ed82d07e23bb
AyushmaanAggarwal <ayu02468@gmail.com>
2023-05-15 11:57:22 -0700

    rewrite javascript for recursive loop, succesfully waits for localhost to open

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import fetch from 'node-fetch';
-import electron from 'electron';
-
 const electron = require( "electron" );
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -18,17 +15,17 @@ app.on(
     "ready",
     function()
     {
-        var subpy = require( "child_process" ).spawn( "python", [ "./app.py" ] );
-        // var subpy = require( "child_process" ).spawn( "./dist/hello.exe" );
+        var subpy = require( "child_process" ).spawn( "python", [ "./website/app.py" ] );
         const fetch = require("node-fetch");
-        const mainAddr = "http://127.0.0.1:5000";
-        var countLog = 50;
+        const mainAddr = "http://127.0.0.1:3213";
+        const maxLogCount = 100;
+        var countLog = maxLogCount;
         
         var OpenWindow = function()
         {
             mainWindow = new BrowserWindow( { width: 800, height: 600 } );
             // mainWindow.loadURL( "file://" + __dirname + "/index.html" );
-            mainWindow.loadURL( "http://localhost:5000" );
+            mainWindow.loadURL( "http://localhost:3213" );
             // mainWindow.webContents.openDevTools();
             mainWindow.menuBarVisible = false;
             mainWindow.on(
@@ -41,22 +38,21 @@ app.on(
                 );
         };
         
-        var StartUp = function()
+        function StartUp()
         {
-            fetch(final_url, params)
-              .then((htmlString) => {
-                  console.log( "server started!" );
-                  OpenWindow();
-              })
-              .catch((err) => {
-                  if (countLog > 50) {
-                      console.log( "Waiting for the server start..." );
-                      countLog = 0;
-                  }
-                  countLog = countLog + 1;
-                  // without tail call optimization this is a potential stack overflow
-                  StartUp();
-              });
+            fetch(mainAddr)
+            .then(res => {
+                console.log( "server started!" );
+                OpenWindow();
+            })
+            .catch(e => {
+                StartUp();
+                if (countLog >= maxLogCount) {
+                        console.log( "Waiting for the server start..." );
+                        countLog = 0;
+                    }
+                countLog = countLog + 1;
+            });
         };
 
         // fire!
