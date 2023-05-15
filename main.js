dcfe657d80478cff5d60eba241f4bf6eb5d9305f
AyushmaanAggarwal <ayu02468@gmail.com>
2023-05-15 13:41:21 -0700

    package flask website using pyinstaller

--- main.js
+++ main.js
@@ -15,9 +15,37 @@ app.on(
     "ready",
     function()
     {
-        var subpy = require( "child_process" ).spawn( "python", [ "./website/app.py" ] );
+        // var subpy = require( "child_process" ).spawn( "python", [ "./website/app.py" ] );
+
+        //        subpy.stdout.on('data', function (data) {
+        //            console.log("data: ", data.toString('utf8'));
+        //        });
+        //        subpy.stderr.on('data', (data) => {
+        //            console.log(`stderr: ${data}`); // when error
+        //        });
+        var subpy = require('child_process').spawn(process.cwd()+"/app");
+        subpy.stdout.on('data', function (data) {
+            console.log("data: ", data.toString('utf8'));
+        });
+        subpy.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`); // when error
+        });
+        //        let backend;
+        //        backend = process.cwd() + '/app'
+        //        var execfile = require('child_process').execFile;
+        //        execfile(
+        //            backend,
+        //            { windowsHide: true,},
+        //            (err, stdout, stderr) => {  if (err)
+        //            {
+        //                console.log(err);
+        //            }  if (stdout) {
+        //                console.log(stdout);
+        //            }  if (stderr) {
+        //                console.log(stderr);
+        //            }})
         const fetch = require("node-fetch");
-        const mainAddr = "http://127.0.0.1:3213";
+        const mainAddr = "http://127.0.0.1:3214";
         const maxLogCount = 100;
         var countLog = maxLogCount;
         
@@ -25,7 +53,7 @@ app.on(
         {
             mainWindow = new BrowserWindow( { width: 800, height: 600 } );
             // mainWindow.loadURL( "file://" + __dirname + "/index.html" );
-            mainWindow.loadURL( "http://localhost:3213" );
+            mainWindow.loadURL( mainAddr );
             // mainWindow.webContents.openDevTools();
             mainWindow.menuBarVisible = false;
             mainWindow.on(
@@ -34,6 +62,14 @@ app.on(
                 {
                     mainWindow = null;
                     subpy.kill( "SIGINT" );
+                    //                    const { exec } = require('child_process');
+                    //                    execfile.kill('SIGINT')
+                    //                    exec('taskkill /f /t /im app', (err, stdout, stderr) => { if (err) {
+                    //                        console.log(err);
+                    //                        return; }
+                    //                        console.log(`stdout: ${stdout}`);
+                    //                        console.log(`stderr: ${stderr}`);
+                    //                    });
                 }
                 );
         };
--- website/app.py
+++ website/app.py
@@ -9,9 +9,9 @@ def home():
     if request.method == 'POST':
         copied_text = request.form['copiedText']
         text = converter(copied_text)
-        return render_template('boostraped_home.html', output_txt=text, input_txt=copied_text)
-    return render_template('boostraped_home.html', output_txt="", input_txt="")
+        return render_template('bootstraped_home.html', output_txt=text, input_txt=copied_text)
+    return render_template('bootstraped_home.html', output_txt="", input_txt="")
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=3213)
+    app.run(host='127.0.0.1', port=3214, debug=False)
